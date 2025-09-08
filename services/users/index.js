c5fc57f284db1b9aafbf9c3bbeed6754adf3d17a
olamiwale <olamiwalepaul@gmail.com>
2025-09-08 02:58:17 +0100

    8080

--- services/users/index.js
+++ services/users/index.js
@@ -1,24 +1,31 @@
+const e = require("express");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
+
 app.use(express.json());
 
-// Health check
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Users Service");
+});
+
+
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok", service: "users-service" });
 });
 
-// Example route
+
 app.get("/users", (req, res) => {
   res.json(
     [
-      { id: 1, name: "John Doe"},
-      { id: 2, name: "Paul Smith"},
-      { id: 3, name: "Jane Doe"},
-      { id: 4, name: "Alice Johnson"},
-      { id: 5, name: "Bob Brown"}
+      { id: 1, name: "John Doe", email: "johnDoe@mail.com", age: 30, role: "admin"},
+      { id: 2, name: "Paul Smith", email: "paulSmith@mail.com", age: 25, role: "user"},
+      { id: 3, name: "Jane Doe", email: "janeDoe@mail.com", age: 28, role: "user"},
+      { id: 4, name: "Alice Johnson",email: "aliceJohnson@mail.com", age: 32, role: "user"},
+      { id: 5, name: "Bob Brown", email: "bobBrown@mail.com",  age: 22, role: "user"},
+      
     ]);
 });
 
